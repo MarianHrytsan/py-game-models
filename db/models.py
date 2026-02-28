5bacb46cd063d07c030aa51920132c8f76145ed2
Marian Hrytsan <gricanmaran@gmail.com>
2026-02-28 03:21:26 +0200

    Corect v1.2

--- db/models.py
+++ db/models.py
@@ -9,7 +9,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="current_rase")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="current_race")
 
 
 class Guild(models.Model):
--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main() -> None:
         players = json.load(file)
     for player_name, player in players.items():
         try:
-            race = Race.objects.get(name=player["race"]["name"])
+            race = Race.objects.get(name=player.get("race").get("name"))
         except Exception:
-            race = Race.objects.create(name=player["race"]["name"],
-                                       description=player["race"]["description"])
+            race = Race.objects.create(name=player.get("race").get("name"),
+                                       description=player.get("race").get("description"))
         try:
             guild = Guild.objects.get(name=player["guild"]["name"])
         except TypeError:
@@ -20,11 +20,11 @@ def main() -> None:
         except Exception:
             guild = Guild.objects.create(name=player["guild"]["name"],
                                          description=player["guild"]["description"])
-        for skill_data in player["race"]["skills"]:
+        for skill_data in player.get("race").get("skills"):
             try:
-                skill = Skill.objects.get(name=skill_data["name"])
+                skill = Skill.objects.get(name=skill_data.get("name"))
             except Exception:
-                skill = Skill.objects.create(name=skill_data["name"],bonus=skill_data["bonus"], race=race)
+                skill = Skill.objects.create(name=skill_data.get("name"),bonus=skill_data.get("bonus"), race=race)
         Player.objects.create(nickname=player_name,
                               email=player["email"],
                               bio=player["bio"],
